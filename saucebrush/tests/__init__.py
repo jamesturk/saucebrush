b47cc70d92aa809a642fa7f923f8eb09466595c0
Jeremy Carbaugh <jcarbaugh@gmail.com>
2011-06-03 10:56:16 -0400

    add basic tests for stats filters

--- saucebrush/tests/__init__.py
+++ saucebrush/tests/__init__.py
@@ -3,11 +3,13 @@ from saucebrush.tests.filters import FilterTestCase
 from saucebrush.tests.sources import SourceTestCase
 from saucebrush.tests.emitters import EmitterTestCase
 from saucebrush.tests.recipes import RecipeTestCase
+from saucebrush.tests.stats import StatsTestCase
 
 filter_suite = unittest.TestLoader().loadTestsFromTestCase(FilterTestCase)
 source_suite = unittest.TestLoader().loadTestsFromTestCase(SourceTestCase)
 emitter_suite = unittest.TestLoader().loadTestsFromTestCase(EmitterTestCase)
 recipe_suite = unittest.TestLoader().loadTestsFromTestCase(RecipeTestCase)
+stats_suite = unittest.TestLoader().loadTestsFromTestCase(StatsTestCase)
 
 if __name__ == '__main__':
     unittest.main()
